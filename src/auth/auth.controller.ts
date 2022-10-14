dc71197fdebdd4c101e2af2c3ee68a8f442536a0
Abdelhamid El kemmal <abdel-ke@e2r5p11.1337.ma>
2022-10-14 17:28:16 +0100

    ok

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { AuthService } from './auth.service';
+import { GetCurrentUser, GetCurrentUserId, Public } from './common/decorator';
+import { AtGuard, RtGuard } from './common/guards';
 import { AuthDto } from './dto';
 import { Token } from './types';
 
@@ -8,29 +11,33 @@ import { Token } from './types';
 export class AuthController {
   constructor(private authservice: AuthService) {}
 
+  @Public()
   @Post('local/signup')
   @HttpCode(HttpStatus.CREATED)
   signupLocal(@Body() dto: AuthDto): Promise<Token> {
     return this.authservice.signupLocal(dto);
   }
 
+  @Public()
   @Post('local/signin')
   @HttpCode(HttpStatus.OK)
   signinLocal(@Body() dto: AuthDto): Promise<Token> {
     return this.authservice.signinLocal(dto);
   }
 
+  // @UseGuards(AtGuard)
   @Post('logout')
   @HttpCode(HttpStatus.OK)
-  logout(@Req() req: Request) {
-    const user = req.user;
-    return this.authservice.logout(user['id']);
+  logout(@GetCurrentUserId() userId: number) {
+    return this.authservice.logout(userId);
   }
 
+  @Public()
+  @UseGuards(RtGuard)
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
-  refreshToken(@Req() req: Request) {
-    const user = req.user;
-    return this.authservice.refreshToken(user['id'], user['refreshToken']);
+  refreshToken(@GetCurrentUserId() userId: number, @GetCurrentUser('refreshToken') refreshToken: string) {
+    // console.log("user: ", userId);
+    return this.authservice.refreshToken(userId['sub'], refreshToken);
   }
 }
--- src/strategies/rt.strategy.ts
+++ src/strategies/rt.strategy.ts
@@ -1,20 +1,35 @@
-import { PassportStrategy } from "@nestjs/passport";
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { Request } from "express";
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import { ForbiddenException } from '@nestjs/common';
+
+type JwtPayload = {
+  sub: string;
+  email: string;
+};
 
 export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
-    constructor() {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: 'rt-secret',
-            passReqToCallback: true,
-        });
-    }
-    validate(req: Request, payload: any) {
-        const refreshToken = req.get('authorization').replace('Bearer', '').trim();
-        return {
-            ...payload,
-            refreshToken
-        };
-    }
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'rt-secret',
+      passReqToCallback: true,
+    });
+  }
+  validate(req: Request, payload: JwtPayload) {
+    const refreshToken = req.get('authorization').replace('Bearer', '').trim();
+    console.log('req: ', req);
+    console.log('payload: ', payload);
+    //     const refreshToken = req
+    //   ?.get('authorization')
+    //   ?.replace('Bearer', '')
+    //   .trim();
+
+    if (!refreshToken) throw new ForbiddenException('Refresh token malformed');
+
+    return {
+      ...payload,
+      refreshToken,
+    };
+  }
 }
